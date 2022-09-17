2a0a568f6846fc15d455df6ad55e36e63116f8f2
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-17 07:32:04 +0300

    Trying R S

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { QueryClientProvider, QueryClient } from 'react-query';
 import App from './App';
+
+const queryClient = new QueryClient();
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
   </React.StrictMode>
 );
