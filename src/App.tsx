c185f09185cffce2bd2fa3c7aa0144a28984419e
Stephen Muchendu <muchendumaina66@gmail.com>
2022-09-19 02:34:50 +0300

    Getting Started

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,38 @@ import axios from 'axios';
 import './App.css';
 
 function App() {
-  const apiKey = '535f82aa5aaf100cf054ac22471e1a03';
-  const lat = 33.44;
-  const lon = -94.04;
-  const { isLoading, error, data } = useQuery(['wetherData'], () => {
-    return axios.get(
-      `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${lon}&exclude=hourly,daily&appid=${apiKey}`
-    );
-  });
+  const successCallback = (position : any) => {
+    console.log(position);
+  };
 
-  console.log(data);
-  console.log(error);
+  const errorCallback = (error : any) => {
+    console.log(error);
+  };
+
+  navigator.geolocation.getCurrentPosition(successCallback, errorCallback);
+  
+  const options = {
+    method: 'GET',
+    url: 'https://weatherbit-v1-mashape.p.rapidapi.com/current',
+    params: { lon: '37.147176', lat: '-0.717751' },
+    headers: {
+      'X-RapidAPI-Key': '3b32d1897bmshd900876b274af28p1d46e5jsn0a440868f822',
+      'X-RapidAPI-Host': 'weatherbit-v1-mashape.p.rapidapi.com'
+    }
+  };
+
+  //fetching data from the API using fetch
+  const axios = require('axios');
+
+  // fetch the data and store it in a variable
+  const getData = async () => {
+    try {
+      const response = await axios.request(options);
+      console.log(response.data);
+    } catch (error) {
+      console.error(error);
+    }
+  };
 
   return <div className='App'></div>;
 }
